03a6bfa492f62798dd5406b79530505d2a1eecda
Daniel <danielac@protonmail.com>
2020-04-18 18:09:56 -0600

    saves progress

--- client/src/views/Ledger.vue
+++ client/src/views/Ledger.vue
@@ -4,10 +4,10 @@
 			<div class="col">
 				<h1 class="pt-5 pb-4">Ledgers</h1>
 				<h5>Future feature: View transaction history for each crypto.</h5>
-				<p>
-					Prior to feature being added, all transactions
-					<br />created will be saved for future display.
-				</p>
+				<div v-for="tx in tx" :key="tx.id">
+					<!-- notes inserted here -->
+					<p>{{tx.cryptoAmount}}</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -17,7 +17,12 @@
 export default {
 	name: "ledger",
 	mounted() {
-		this.$store.dispatch("getTxByCrypto", this.$route.params.id);
+		this.$store.dispatch("getTxByCrypto", this.$route.params.cryptoId);
+	},
+	computed: {
+		tx() {
+			return this.$store.state.tx;
+		}
 	}
 };
 </script>
