b0be0f1da3807eb6966f97f4bbcc0459b16bcb3e
Daniel <danielac@protonmail.com>
2020-04-24 19:19:11 -0600

    saves progress

---
+++ client/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="about container">
+		<div class="row">
+			<div class="col-12">
+				<h4>Why Sign Up?</h4>
+			</div>
+			<div class="col-12 col-sm-6 text-left">
+				<p></p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "about",
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style>
+</style>
