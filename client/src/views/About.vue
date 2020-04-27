c34a2103ddac04042c705bcfeff42ac6c839250e
Daniel <danielac@protonmail.com>
2020-04-27 16:39:22 -0600

    adds about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,10 +2,90 @@
 	<div class="about container">
 		<div class="row">
 			<div class="col-12">
-				<h4>Why Sign Up?</h4>
+				<h1 class="pt-5 pb-2">Why Sign Up?</h1>
 			</div>
-			<div class="col-12 col-sm-6 text-left">
-				<p></p>
+			<div class="col-12 col-lg-7 text-left">
+				<p>One of the biggest problems facing cryptocurrency users and slowing down its adoption is its fluctuating value relative to local currency. If you bought one Bitcoin for $8,600 then made a purchase the next day when the price was $8,300, you would lose purchasing power. In order to keep that from happening, you need to wait to spend spend any of that Bitcoin until the Bitcoin market price is at or above what you paid for it.</p>
+				<p>When dealing with one crypto purchase, it is easy to know when that is. But what about when you have multiple coins and tokens to track and you make multiple purchases of them all at different values? You would have to use a spreadsheet app to track all of your transaction data, set up the calculations for tracking the rolling average of each crypto's value, then store that document on the cloud for easy access.</p>
+				<p>Or you could use WhenToCrypto.</p>
+			</div>
+		</div>
+		<!-- Account features section -->
+		<div class="row pt-5">
+			<div class="col-12">
+				<h3 class="pb-2">Account Features</h3>
+			</div>
+			<div class="col-12">
+				<div class="row">
+					<div class="col-12 col-sm-6 text-left">
+						<h5 class="text-center">Unpaid</h5>
+						<ul>
+							<li>Track unlimited transactions for one cryptocurrency</li>
+							<li>View your crypto's...</li>
+							<ul>
+								<li>Averaged value</li>
+								<li>Total balance</li>
+								<li>Last 13 months of transaction history which can easily be copied to a spreadsheet for long-term record keeping</li>
+							</ul>
+							<li>Access a conveniently placed link to your crypto's market value</li>
+							<li>Edit the description, picture, notes, and name of your tracked crypto</li>
+							<li>Enter the date, type, amount, local currency value, and notes of each transaction</li>
+						</ul>
+					</div>
+					<div class="col-12 col-sm-6 text-left">
+						<h5 class="text-center">Subscription</h5>
+						<ul>
+							<li>All features of unpaid accounts</li>
+							<li>Ability to track any number of cryptos</li>
+						</ul>
+						<strong>Note:</strong>
+						<p>In the case of non-renewal of subscriptions or lapse in payment, all crypto's transaction histories will still be viewable going back 13 months. However, transactions will only be able to be added to the account's primary crypto.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Payment info section -->
+		<div class="row pt-5">
+			<div class="col-12">
+				<h3 class="pb-2 text-center">Payment</h3>
+			</div>
+			<div class="col-12">
+				<div class="row">
+					<div class="col-12 col-sm-6 text-left">
+						<p>WhenToCrypto only accepts cryptocurrency for payment in order to provide secure payment and to encourage the everyday use of crypto. Before the full version of WhenToCrypto is released, there will be payment tab added to the navbar which logged in users will be able to access.</p>
+						<p>In order to pay, users will send their payment to any of the addresses listed then send an email to whentocrypto@protonmail.com containing the transaction id. In case of the unlikely event of another user claiming the payment as their own, it is advised that the email be sent immediately following the start of the transaction.</p>
+					</div>
+					<div class="col-12 col-sm-6 text-left">
+						<h5 class="text-center">Accepted Coins</h5>
+						<div class="row">
+							<div class="col-6">
+								<ul>
+									<li>Bitcoin</li>
+									<li>Bitcoin Cash</li>
+									<li>Zcash</li>
+								</ul>
+							</div>
+							<div class="col-6">
+								<ul>
+									<li>Litecoin</li>
+									<li>Ethereum</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Beta sign-up bonus section -->
+		<div class="row pt-5">
+			<div class="col-12">
+				<h3 class="pb-2">Bonus for Signing Up Now During Beta</h3>
+				<div class="col-12 col-lg-7 text-left">
+					<p>
+						As a thank you for signing up during beta testing and (if you would like) sending emailing me feedback, you will get 3, free months of subscription.
+						If you don't already have a coupon code, please mention how you heard about WhenToCrypto in the Coupon Code field on the registration form.
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
--- client/src/views/Features.vue
+++ client/src/views/Features.vue
@@ -2,7 +2,7 @@
 	<div class="features container">
 		<div class="row">
 			<div class="col-12 text-center">
-				<h1>Planned Features and Fixes</h1>
+				<h1 class="pt-5">Planned Features and Fixes</h1>
 			</div>
 			<div class="col-12 text-left pt-5 pb-5">
 				<ol>
