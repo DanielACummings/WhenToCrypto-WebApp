0536ced2039e19768578b0d8d600d2a3c4bdc027
Daniel <danielac@protonmail.com>
2020-05-14 15:28:23 -0600

    combines crypto and metal transaction btns into 1

--- client/src/components/AddCryptoTxForm.vue
+++ client/src/components/AddCryptoTxForm.vue
@@ -1,156 +1,124 @@
 <template>
-	<div class="add-crypto-tx-form">
-		<button
-			type="button"
-			class="btn btn-primary text-center"
-			data-toggle="modal"
-			data-target="#add-transaction-modal"
-			data-whatever="@getbootstrap"
-		>Add Crypto Transaction</button>
-		<div
-			class="modal fade"
-			id="add-transaction-modal"
-			tabindex="-1"
-			role="dialog"
-			aria-labelledby="add-transaction-modal-label"
-			aria-hidden="true"
-		>
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<div class="modal-title" id="add-transaction-modal-label">
-							<h5>Add a Crypto Transaction</h5>
-						</div>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-						<form @submit.prevent="createTransaction" class="text-left">
-							<!-- "Which Crypto?" dropdown -->
-							<div class="form-group">
-								<label for="which-crypto" class="col-form-label">Which Crypto?</label>
-								<select id="which-crypto" class="form-control" v-model="newCryptoTransaction.cryptoId">
-									<option value disabled selected>-</option>
-									<option
-										v-for="crypto in cryptos"
-										:key="crypto.id"
-										:value="crypto.id"
-									>{{crypto.name}} - {{crypto.description || "No description"}}</option>
-								</select>
-							</div>
-
-							<!-- Transaction type dropdown -->
-							<div class="form-group">
-								<label for="transaction-type" class="col-form-label">Type:</label>
-								<select
-									v-model="newCryptoTransaction.transactionType"
-									class="form-control"
-									id="transaction-type"
-									required
-								>
-									<!-- Placeholder -->
-									<option value disabled selected>-</option>
-									<!-- Positive -->
-									<option>Purchased with Local Currency</option>
-									<option>Income</option>
-									<option>Earned as Payment</option>
-									<option>Received as Gift</option>
-									<option>Hard Fork</option>
-									<option>Interest</option>
-									<option>Mining</option>
-									<option>Airdrop</option>
-									<!-- Negative -->
-									<option>Used for Payment</option>
-									<option>Gave as Gift</option>
-									<option>Sold for Local Currency</option>
-									<option>Mining Fee for Wallet Transfer</option>
-									<option>Lost or Stolen</option>
-									<!-- Either -->
-									<option>Conversion Between Cryptos</option>
-									<option>Other</option>
-								</select>
-							</div>
-
-							<!-- Date -->
-							<div class="form-group">
-								<label for="date" class="col-form-label">Date:</label>
-								<input
-									type="text"
-									v-model="newCryptoTransaction.date"
-									class="form-control"
-									id="date"
-									placeholder="yyyy/mm/dd"
-									required
-								/>
-							</div>
-							<!-- +/- radio buttons for adding or subtracting from ledger-->
-							<div class="form-group">
-								<label for="pos-or-neg" class="col-form-label">Adding or Subtracting from Ledger?</label>
-								<br />
-								<input
-									type="radio"
-									v-model="newCryptoTransaction.posOrNeg"
-									id="pos"
-									name="pos-or-neg"
-									value="pos"
-									required
-								/>
-								<label for="pos">Adding</label>
-								<input
-									type="radio"
-									v-model="newCryptoTransaction.posOrNeg"
-									id="neg"
-									name="pos-or-neg"
-									value="neg"
-								/>
-								<label for="neg">Subtracting</label>
-							</div>
-							<div class="form-group">
-								<label for="crypto-amount" class="col-form-label">Amount of Crypto:</label>
-								<input
-									type="number"
-									step="any"
-									min="0"
-									v-model="newCryptoTransaction.cryptoAmount"
-									class="form-control"
-									id="crypto-amount"
-									required
-								/>
-							</div>
-							<div class="form-group">
-								<label for="market-value" class="col-form-label">
-									Amount of Local Currency:
-									<br />(Including Fees)
-								</label>
-								<input
-									type="number"
-									step="any"
-									min="0"
-									v-model="newCryptoTransaction.marketValue"
-									class="form-control"
-									id="market-value"
-									required
-								/>
-							</div>
-							<div class="form-group">
-								<label for="notes" class="col-form-label">Notes:</label>
-								<input
-									type="text"
-									v-model="newCryptoTransaction.notes"
-									class="form-control"
-									id="notes"
-									placeholder="Optional"
-								/>
-							</div>
-							<button type="submit" class="btn btn-primary">Submit</button>
-							<button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
-						</form>
-					</div>
-				</div>
-			</div>
+	<form @submit.prevent="createTransaction" class="text-left">
+		<!-- "Which Crypto?" dropdown -->
+		<div class="form-group">
+			<label for="which-crypto" class="col-form-label">Which Crypto?</label>
+			<select id="which-crypto" class="form-control" v-model="newCryptoTransaction.cryptoId">
+				<option value disabled selected>-</option>
+				<option
+					v-for="crypto in cryptos"
+					:key="crypto.id"
+					:value="crypto.id"
+				>{{crypto.name}} - {{crypto.description || "No description"}}</option>
+			</select>
 		</div>
-	</div>
+
+		<!-- Transaction type dropdown -->
+		<div class="form-group">
+			<label for="transaction-type" class="col-form-label">Type:</label>
+			<select
+				v-model="newCryptoTransaction.transactionType"
+				class="form-control"
+				id="transaction-type"
+				required
+			>
+				<!-- Placeholder -->
+				<option value disabled selected>-</option>
+				<!-- Positive -->
+				<option>Purchased with Local Currency</option>
+				<option>Income</option>
+				<option>Earned as Payment</option>
+				<option>Received as Gift</option>
+				<option>Hard Fork</option>
+				<option>Interest</option>
+				<option>Mining</option>
+				<option>Airdrop</option>
+				<!-- Negative -->
+				<option>Used for Payment</option>
+				<option>Gave as Gift</option>
+				<option>Sold for Local Currency</option>
+				<option>Mining Fee for Wallet Transfer</option>
+				<option>Lost or Stolen</option>
+				<!-- Either -->
+				<option>Conversion Between Cryptos</option>
+				<option>Other</option>
+			</select>
+		</div>
+
+		<!-- Date -->
+		<div class="form-group">
+			<label for="date" class="col-form-label">Date:</label>
+			<input
+				type="text"
+				v-model="newCryptoTransaction.date"
+				class="form-control"
+				id="date"
+				placeholder="yyyy/mm/dd"
+				required
+			/>
+		</div>
+		<!-- +/- radio buttons for adding or subtracting from ledger-->
+		<div class="form-group">
+			<label for="pos-or-neg" class="col-form-label">Adding or Subtracting from Ledger?</label>
+			<br />
+			<input
+				type="radio"
+				v-model="newCryptoTransaction.posOrNeg"
+				id="pos"
+				name="pos-or-neg"
+				value="pos"
+				required
+			/>
+			<label class="pr-4" for="pos">Adding</label>
+			<input
+				type="radio"
+				v-model="newCryptoTransaction.posOrNeg"
+				id="neg"
+				name="pos-or-neg"
+				value="neg"
+			/>
+			<label for="neg">Subtracting</label>
+		</div>
+		<div class="form-group">
+			<label for="crypto-amount" class="col-form-label">Amount of Crypto:</label>
+			<input
+				type="number"
+				step="any"
+				min="0"
+				v-model="newCryptoTransaction.cryptoAmount"
+				class="form-control"
+				id="crypto-amount"
+				required
+			/>
+		</div>
+		<div class="form-group">
+			<label for="market-value" class="col-form-label">
+				Amount of Local Currency:
+				<br />(Including Fees)
+			</label>
+			<input
+				type="number"
+				step="any"
+				min="0"
+				v-model="newCryptoTransaction.marketValue"
+				class="form-control"
+				id="market-value"
+				required
+			/>
+		</div>
+		<div class="form-group">
+			<label for="notes" class="col-form-label">Notes:</label>
+			<input
+				type="text"
+				v-model="newCryptoTransaction.notes"
+				class="form-control"
+				id="notes"
+				placeholder="Optional"
+			/>
+		</div>
+		<button type="submit" class="btn btn-primary">Submit</button>
+		<button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+	</form>
 </template>
 
 <script>
---
+++ client/src/components/CreateTxModal.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="create-tx-modal">
+		<button
+			type="button"
+			class="btn btn-primary text-center"
+			data-toggle="modal"
+			data-target="#create-tx-modal"
+			data-whatever="@getbootstrap"
+		>Add Transaction</button>
+		<div
+			class="modal fade"
+			id="create-tx-modal"
+			tabindex="-1"
+			role="dialog"
+			aria-labelledby="create-tx-modal-label"
+			aria-hidden="true"
+		>
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<div class="modal-title" id="create-tx-modal-label">
+							<h5>Add Transaction</h5>
+						</div>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form>
+							<input v-model="currencyType" type="radio" name="crypto-or-metal" value="crypto" />
+							<label class="pr-5" for="crypto">Crypto</label>
+							<input v-model="currencyType" type="radio" name="crypto-or-metal" value="metal" />
+							<label for="metal">Metal</label>
+						</form>
+						<!-- Add crypto transaction form -->
+						<div v-if="currencyType == 'crypto'">
+							<addCryptoTxForm />
+						</div>
+						<!-- Add metal transaction form -->
+						<div v-if="currencyType == 'metal'">
+							<addMetalTxForm />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import addCryptoTxForm from "./AddCryptoTxForm";
+import addMetalTxForm from "./AddMetalTxForm";
+
+export default {
+	name: "createTxForm",
+	components: {
+		addCryptoTxForm,
+		addMetalTxForm
+	},
+	data() {
+		return {
+			currencyType: ""
+		};
+	}
+};
+</script>
+
+<style>
+</style>
